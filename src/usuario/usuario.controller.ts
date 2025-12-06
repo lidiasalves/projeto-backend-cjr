16a2f5dd7711344ed00ced1bc04f3dd8b9850bbc
AFRS0311 <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>
2025-12-06 20:29:41 -0300

    migration e fix no usuario module

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,7 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/require-await */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable prettier/prettier */
 import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [UsuarioService],
   controllers: [UsuarioController],
   exports: [UsuarioService],
